35c118d47e25c76068714d762e859e73e270b99f
iyanuhacks <iyanuhacks@gmail.com>
2023-10-21 17:05:50 +0100

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 import random
 import string
 
@@ -8,19 +9,28 @@ app = Flask(__name__)
 def home():
   return render_template("home.html")
 
-@app.route('/power')
+@app.route('/power/power.html')
 def power():
   return render_template("power.html")
 
-@app.route('/anime')
+@app.route('/anime/anime.html')
 def anime():
   return render_template("anime.html")
+
+@app.route('/loading/loading',methods=['POST'])
+def loading():
+  return render_template("loading.html")
+
+@app.template_filter('timeformat2')
+def timeformat2(s):
+    return time.strftime('%S', time.gmtime(s))
+
 
 @app.template_filter('timeformat')
 def timeformat(s):
     return time.strftime('%M:%S', time.gmtime(s))
 
-@app.route('/index',methods=['POST'])
+@app.route('/index') #,methods=['POST'])
 def index():
     account_name = generate_account_name()
     account_number = generate_account_number()
@@ -39,6 +49,10 @@ def generate_bank_name():
     bank = "Palmpay"
     return bank
 
+freezer = Freezer(app)
+
+
 if __name__ == '__main__':
+#    freezer.freeze()
     app.run(debug=False)
 
